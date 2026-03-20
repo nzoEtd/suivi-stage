66e980efe29d303c69c4ba88be391a598c82b816
Zelie Davaud <zelie.davaud@gmail.com>
2026-03-20 11:09:45 +0100

    Personnal env variables gitignored

--- DOCKER/php/angular/appli-test/src/environments/environment.prod.ts
+++ DOCKER/php/angular/appli-test/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:
-    "http://lakartxela.iutbayonne.univ-pau.fr/~zdavaud/S5.01/GestionDesStages2/public",
-  logoIUT:
-    "/~zdavaud/S5.01/GestionDesStages2/public/frontend/logo_iutbpb_2018.png ",
+  apiUrl: "",
+  logoIUT: "/suivi-stage/public/frontend/logo_iutbpb_2018.png",
 };
