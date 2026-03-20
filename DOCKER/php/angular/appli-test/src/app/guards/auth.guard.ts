b74dbaf251e115f347d2d0d4e6e22ec6a3000086
Zelie Davaud <zelie.davaud@gmail.com>
2026-03-20 10:45:55 +0100

    Use env variables for angular deployment

--- DOCKER/php/angular/appli-test/src/app/guards/auth.guard.ts
+++ DOCKER/php/angular/appli-test/src/app/guards/auth.guard.ts
@@ -1,44 +1,51 @@
-import { inject } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { firstValueFrom } from 'rxjs';
+import { inject } from "@angular/core";
+import { Router, ActivatedRouteSnapshot, CanActivateFn } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { firstValueFrom } from "rxjs";
+import { environment } from "../../environments/environment";
 
-export const authGuard: CanActivateFn = async (route: ActivatedRouteSnapshot, state) => {
+export const authGuard: CanActivateFn = async (
+  route: ActivatedRouteSnapshot,
+  state,
+) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const expectedRole = route.data['role'];
-  const isDashboardRoute = state.url.includes('/dashboard');
-  const isFactsheetRoute = state.url.includes('/factsheets');
+  const expectedRole = route.data["role"];
+  const isDashboardRoute = state.url.includes("/dashboard");
+  const isFactsheetRoute = state.url.includes("/factsheets");
 
   const user = await firstValueFrom(authService.getAuthenticatedUser());
 
   if (Array.isArray(user) && user.length === 0) {
-    window.location.href = 'http://localhost:8000/api/cas-auth';
+    window.location.href = `${environment.apiUrl}/api/cas-auth`;
     return false;
   }
 
   const currentUserRole = authService.isStudent(user)
-    ? 'STUDENT'
+    ? "STUDENT"
     : authService.isStaff(user)
-    ? 'INTERNSHIP_MANAGER'
-    : null;
+      ? "INTERNSHIP_MANAGER"
+      : null;
 
   if (expectedRole && currentUserRole !== expectedRole) {
-    if (currentUserRole === 'INTERNSHIP_MANAGER') {
-      const selectedStudent = sessionStorage.getItem('selectedStudent');
+    if (currentUserRole === "INTERNSHIP_MANAGER") {
+      const selectedStudent = sessionStorage.getItem("selectedStudent");
       if (selectedStudent && isDashboardRoute) {
         const selectedStudentId = JSON.parse(selectedStudent).idUPPA;
-        const idSearch = route.paramMap.get('idSearch');
-        return router.createUrlTree([`/dashboard/student-dashboard/${selectedStudentId}/search-details/${idSearch}`]);
-      }
-      else if (selectedStudent && isFactsheetRoute) {
+        const idSearch = route.paramMap.get("idSearch");
+        return router.createUrlTree([
+          `/dashboard/student-dashboard/${selectedStudentId}/search-details/${idSearch}`,
+        ]);
+      } else if (selectedStudent && isFactsheetRoute) {
         const selectedStudentId = JSON.parse(selectedStudent).idUPPA;
-        const idSheet = route.paramMap.get('idSheet');
-        return router.createUrlTree([`/factsheets/student-factsheets/${selectedStudentId}/sheet-details/${idSheet}`]);
+        const idSheet = route.paramMap.get("idSheet");
+        return router.createUrlTree([
+          `/factsheets/student-factsheets/${selectedStudentId}/sheet-details/${idSheet}`,
+        ]);
       }
     }
-    return router.createUrlTree(['/dashboard']);
+    return router.createUrlTree(["/dashboard"]);
   }
 
   return true;
-}
+};
--- DOCKER/php/angular/appli-test/src/environments/environment.prod.ts
+++ DOCKER/php/angular/appli-test/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    production: true,
-    apiUrl: '',
-    logoIUT: '/suivi-stage/public/frontend/logo_iutbpb_2018.png'
+  production: true,
+  apiUrl:
+    "http://lakartxela.iutbayonne.univ-pau.fr/~zdavaud/S5.01/GestionDesStages2/public",
+  logoIUT:
+    "/~zdavaud/S5.01/GestionDesStages2/public/frontend/logo_iutbpb_2018.png ",
 };
