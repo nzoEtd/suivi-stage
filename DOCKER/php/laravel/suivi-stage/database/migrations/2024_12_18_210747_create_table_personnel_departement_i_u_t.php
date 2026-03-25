e1a9abce8ac4c98c96e3c27ee3a337af3eac96ed
Zelie Davaud <zelie.davaud@gmail.com>
2026-03-25 09:02:47 +0100

    OnDeleteCascade to Personnel FK

--- DOCKER/php/laravel/suivi-stage/database/migrations/2024_12_18_210747_create_table_personnel_departement_i_u_t.php
+++ DOCKER/php/laravel/suivi-stage/database/migrations/2024_12_18_210747_create_table_personnel_departement_i_u_t.php
@@ -20,10 +20,10 @@ class CreateTablePersonnelDepartementIUT extends Migration
 
             // Clé étrangère
             $table->unsignedInteger('idPersonnel');
-            $table->foreign('idPersonnel')->references('idPersonnel')->on('personnels');
+            $table->foreign('idPersonnel')->references('idPersonnel')->on('personnels')->onDelete('cascade');
 
             $table->unsignedTinyInteger('idDepartement');
-            $table->foreign('idDepartement')->references('idDepartement')->on('departement_i_u_t_s');
+            $table->foreign('idDepartement')->references('idDepartement')->on('departement_i_u_t_s')->onDelete('cascade');
         });
     }
 
