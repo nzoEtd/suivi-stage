a6d671246658dee9c30a3f5727cdad557521525e
Zelie Davaud <zelie.davaud@gmail.com>
2026-03-31 10:38:33 +0200

    New file for slot utils

---
+++ DOCKER/php/angular/appli-test/src/app/utils/slotsUtils.ts
@@ -0,0 +1,12 @@
+import { CreneauDisponible } from "../components/modale-soutenance/modale-soutenance.component";
+
+// sort by day, then room, then hours
+export function sortCreneaux(
+  creneaux: CreneauDisponible[],
+): CreneauDisponible[] {
+  return creneaux.sort((a, b) => {
+    if (a.date !== b.date) return a.date.localeCompare(b.date);
+    if (a.salle !== b.salle) return a.salle - b.salle;
+    return a.heureDebut.localeCompare(b.heureDebut);
+  });
+}
