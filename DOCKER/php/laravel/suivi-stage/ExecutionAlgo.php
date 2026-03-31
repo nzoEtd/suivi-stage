0458dbb4d9f588176e22c210dddf470de8dcc4b2
Zelie Davaud <zelie.davaud@gmail.com>
2026-03-31 09:16:35 +0200

    SQ refactor : clean dead code on ExecutionAlgo

--- DOCKER/php/laravel/suivi-stage/ExecutionAlgo.php
+++ DOCKER/php/laravel/suivi-stage/ExecutionAlgo.php
@@ -9,8 +9,6 @@ use App\AlgorithmeAttribution\AlgorithmeRepartition;
 
 // Vérifier si les deux IDs sont fournis en argument
 if ($argc < 3) {
-    // echo "Usage: php index.php <idUPPA> <idFicheDescriptive>\n";
-    // echo "Exemple: php index.php 610000 42\n";
     exit(1);
 }
 
@@ -18,18 +16,9 @@ $idUPPA = $argv[1];
 $idFicheDescriptive = $argv[2];
 
 try {
-    // echo "Démarrage de l'algorithme pour l'étudiant $idUPPA (Fiche descriptive: $idFicheDescriptive)...\n";
-
     $db = DatabaseConnection::connect();
     $algorithme = new AlgorithmeRepartition($db);
     $resultat = $algorithme->executeForStudent($idUPPA, $idFicheDescriptive);
-
-    if ($resultat) {
-        // echo "Attribution réussie !\n";
-        // print_r($resultat);
-    } else {
-        // echo "Aucune attribution possible.\n";
-    }
 } catch (Exception $e) {
     echo "ERREUR : " . $e->getMessage() . "\n";
 }
